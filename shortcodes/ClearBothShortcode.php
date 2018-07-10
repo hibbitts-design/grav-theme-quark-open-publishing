489141be874d535d1ba4689b1fbdf87e65fbe822
Paul Hibbitts <paul_hibbitts@sfu.ca>
2018-07-10 12:53:08 -0700

    Added experimental ShortCode 'clearboth' to insert Div for clearing image floats.

---
+++ shortcodes/ClearBothShortcode.php
@@ -0,0 +1,22 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Grav\Common\Utils;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class ClearBothShortcode extends Shortcode
+{
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('clearboth', function(ShortcodeInterface $sc) {
+
+            // Get shortcode content and parameters
+            $str = $sc->getContent();
+
+            $output = '<div style="clear: both;"></div>';
+
+            return $output;
+
+        });
+    }
+}
