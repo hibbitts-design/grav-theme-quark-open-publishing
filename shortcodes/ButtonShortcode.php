0b263c36cf4e9d550c1e3100996950ca253d5af2
Paul Hibbitts <paul_hibbitts@sfu.ca>
2018-09-06 15:41:46 -0700

    Added Spectre.CSS Button Shortcode ([button url="" label="" target="" size="" classes=""]).

---
+++ shortcodes/ButtonShortcode.php
@@ -0,0 +1,37 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Grav\Common\Utils;
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class ButtonShortcode extends Shortcode
+{
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('button', function(ShortcodeInterface $sc) {
+
+            // Get shortcode content and parameters
+            $str = $sc->getContent();
+
+            $buttonurl= $sc->getParameter('url', $sc->getBbCode());
+            $buttontarget= $sc->getParameter('target', $sc->getBbCode());
+            $buttonstyle= $sc->getParameter('style', $sc->getBbCode());
+            $buttonsize= $sc->getParameter('size', $sc->getBbCode());
+            $buttonclasses= $sc->getParameter('classes', $sc->getBbCode());
+            $buttonlabel= $sc->getParameter('label', $sc->getBbCode());
+
+            if (empty($buttonsize)) {
+              $buttonsize = "btn";
+            } else {
+              $buttonsize = "btn btn-" . $buttonsize;
+            }
+
+            if (!empty($buttonurl) && !empty($buttonlabel)) {
+                $output = '<a href="'.$buttonurl.'" target="'.$buttontarget.'" class="'.$buttonsize.' btn-'.$buttonstyle.' '.$buttonclasses.'">'.$buttonlabel.'</a>';
+
+                return $output;
+            }
+
+        });
+    }
+}
