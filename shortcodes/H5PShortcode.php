7ff2b72af56175eb724c9d2638056dfc24174fcc
Paul Hibbitts <paul_hibbitts@sfu.ca>
2019-02-06 08:14:14 -0800

    Added support for H5P content to be hosted on H5P.com.

--- shortcodes/H5PShortcode.php
+++ shortcodes/H5PShortcode.php
@@ -18,10 +18,15 @@ class H5PShortcode extends Shortcode
             $h5pid = $sc->getParameter('id', $sc->getBbCode());
 
             if ($h5pid) {
-                $grav = Grav::instance();
-                $h5proot = $grav['config']->get('theme.h5pembedrootpath', 'https://h5p.org/h5p/embed/');
+                $config = Grav::instance()['config'];
+                $h5proot = $config->get('themes.' . $config->get('system.pages.theme') . '.h5pembedrootpath');
 
-                $output = '<iframe src="' . $h5proot . '' . $h5pid . '" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script><br><br>';
+
+                if (strpos($h5proot, 'h5p.com') !== false) {
+                    $output = '<iframe src="'.$h5proot.''.$h5pid.'/embed" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script><br><br>';
+                } else {
+                    $output = '<iframe src="'.$h5proot.''.$h5pid.'" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script><br><br>';
+                }
 
                 return $output;
             }
